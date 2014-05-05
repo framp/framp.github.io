dc235bcc472738a563d303bced2ad6afe751ecd1
Federico Rampazzo <frampone@gmail.com>
2014-05-05 02:20:24 +0100

    color adjustment, better body fonts, back to circular image

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-/*! framp.me 05-05-2014 01:05:38 */
+/*! framp.me 05-05-2014 02:05:20 */
 !function() {
     function a(a, b) {
         return b = b.shiftBlocks(c), a.partial("layout", b, null);
     }
     function b(a, b) {
-        return b = b.shiftBlocks(c), a.write('<div id="tab-hello"><div class="wrapper-vertical"><div class="wrapper-horizontal"><div id="avatar" class="hexagon"><div class="hexagon-in1"><div class="hexagon-in2"><img src="http://gravatar.com/avatar/21fc27a2ac6cd9094a423997f0344a0b?s=320" /></div></div></div><div id="hello"><div id="title">').reference(b._get(!1, [ "title" ]), b, "h").write('</div><p id="description">').reference(b._get(!1, [ "description" ]), b, "h").write('</p><div id="social"><a href="mailto:').reference(b._get(!1, [ "email" ]), b, "h").write('?Subject=Hi%20Framp" class="icon-envelope-alt" target="_blank"></a><a href="').reference(b._get(!1, [ "github" ]), b, "h").write('" class="icon-github" target="_blank"></a><a href="').reference(b._get(!1, [ "bitbucket" ]), b, "h").write('" class="icon-bitbucket" target="_blank"></a><a href="').reference(b._get(!1, [ "facebook" ]), b, "h").write('" class="icon-facebook-sign" target="_blank"></a><a href="').reference(b._get(!1, [ "plus" ]), b, "h").write('" class="icon-google-plus" target="_blank"></a><a href="').reference(b._get(!1, [ "twitter" ]), b, "h").write('" class="icon-twitter" target="_blank"></a><a href="').reference(b._get(!1, [ "linkedin" ]), b, "h").write('" class="icon-linkedin" target="_blank"></a></div></div><div class="clear"></div></div></div></div>');
+        return b = b.shiftBlocks(c), a.write('<div id="tab-hello"><div class="wrapper-vertical"><div class="wrapper-horizontal"><div id="avatar"></div><div id="hello"><div id="title">').reference(b._get(!1, [ "title" ]), b, "h").write('</div><p id="description">').reference(b._get(!1, [ "description" ]), b, "h").write('</p><div id="social"><a href="mailto:').reference(b._get(!1, [ "email" ]), b, "h").write('?Subject=Hi%20Framp" class="icon-envelope-alt" target="_blank"></a><a href="').reference(b._get(!1, [ "github" ]), b, "h").write('" class="icon-github" target="_blank"></a><a href="').reference(b._get(!1, [ "bitbucket" ]), b, "h").write('" class="icon-bitbucket" target="_blank"></a><a href="').reference(b._get(!1, [ "facebook" ]), b, "h").write('" class="icon-facebook-sign" target="_blank"></a><a href="').reference(b._get(!1, [ "plus" ]), b, "h").write('" class="icon-google-plus" target="_blank"></a><a href="').reference(b._get(!1, [ "twitter" ]), b, "h").write('" class="icon-twitter" target="_blank"></a><a href="').reference(b._get(!1, [ "linkedin" ]), b, "h").write('" class="icon-linkedin" target="_blank"></a></div></div><div class="clear"></div></div></div></div>');
     }
     dust.register("index", a);
     var c = {
@@ -19,7 +19,7 @@
         }, null);
     }
     function b(a, b) {
-        return a.write('<!DOCTYPE html><html><head><meta charset="utf-8"/><meta name="viewport" content="width=device-width"/><title>').reference(b._get(!1, [ "title" ]), b, "h").write('</title><link href=\'http://fonts.googleapis.com/css?family=Lato\' rel=\'stylesheet\' type=\'text/css\'><script src="http://code.jquery.com/jquery-1.9.1.min.js"></script><!--<script src="/js/main.js"></script>--><link rel="stylesheet" type="text/css" href="/css/main.css"/></head><body>').block(b.getBlock("main"), b, {
+        return a.write('<!DOCTYPE html><html><head><meta charset="utf-8"/><meta name="viewport" content="width=device-width"/><title>').reference(b._get(!1, [ "title" ]), b, "h").write('</title><link href=\'http://fonts.googleapis.com/css?family=Open+Sans:400,800\' rel=\'stylesheet\' type=\'text/css\'><script src="http://code.jquery.com/jquery-1.9.1.min.js"></script><!--<script src="/js/main.js"></script>--><link rel="stylesheet" type="text/css" href="/css/main.css"/></head><body>').block(b.getBlock("main"), b, {
             block: c
         }, null).write("</body></html>");
     }
