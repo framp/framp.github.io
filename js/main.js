ef8ba35521513dcbfa056c73629121c73d48fdf8
Federico Rampazzo <frampone@gmail.com>
2014-05-29 01:40:54 +0100

    fix avatar loading

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-/*! framp.me 29-05-2014 01:05:20 */
+/*! framp.me 29-05-2014 01:05:16 */
 document.documentElement.className = "", function() {
     var a = new Image();
     a.onload = function() {
         (document.getElementById("avatar") || {}).className += " loaded";
-    }, a.src = "http://gravatar.com/avatar/21fc27a2ac6cd9094a423997f0344a0b?s=320";
+    }, a.src = "http://gravatar.com/avatar/21fc27a2ac6cd9094a423997f0344a0b?s=280";
 }();
--- js/src/hello.js
+++ js/src/hello.js
@@ -3,5 +3,5 @@
   preload.onload = function() {
     (document.getElementById('avatar') || {}).className += ' loaded';
   }
-  preload.src = 'http://gravatar.com/avatar/21fc27a2ac6cd9094a423997f0344a0b?s=320'; 
+  preload.src = 'http://gravatar.com/avatar/21fc27a2ac6cd9094a423997f0344a0b?s=280'; 
 })();
