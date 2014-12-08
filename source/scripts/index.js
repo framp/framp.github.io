99ef3d4066328a6d6f86d975b94fbf8fc8e08334
Federico Rampazzo <frampone@gmail.com>
2014-12-08 21:44:05 +0000

    preloading avatar script

---
+++ source/scripts/index.js
@@ -0,0 +1,1 @@
+require('./tab-hello.js');
---
+++ source/scripts/tab-hello.js
@@ -0,0 +1,8 @@
+document.documentElement.className = '';
+(function() {
+  var a = new Image();
+  a.onload = function() {
+    (document.getElementById("avatar") || {}).className += " loaded";
+  };
+  a.src = "http://gravatar.com/avatar/21fc27a2ac6cd9094a423997f0344a0b?s=280";
+})();
