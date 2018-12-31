2cbe08fd900abfb9c106c79d401c26ee36b9c2a4
Federico Rampazzo <frampone@gmail.com>
2018-12-31 13:35:22 +0000

    Removed service worker

---
+++ src/components/snake/index.js
@@ -0,0 +1,22 @@
+import { Component } from 'preact';
+
+export default class Snake extends Component {
+    constructor(props) {
+        super(props);
+
+        this.canvas = null;
+        this.setCanvasRef = element => {
+            this.canvas = element;
+        };
+    }
+    componentDidMount() {
+        const ctx = this.canvas.getContext('2d');
+        ctx.clearRect(0,0, 300, 300);
+        ctx.fillRect(0,0, 100, 100);
+    }
+    render() {
+            return (
+                <canvas ref={this.setCanvasRef} width={300} height={300}/>
+            );
+    }
+}
